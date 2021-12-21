7a890dc91962df5a09689f538c6d8957e34300ad
sexfat <bryant6831@gmail.com>
2021-12-21 15:22:22 +0800

    add gsap

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ document.getElementById('app').innerHTML = x(10 , 30)
 // jquery import
 import $ from 'jquery';
 $('body').css('background-color' , 'yellow');
+
+
+//  gsap  import
+import {gsap} from 'gsap';
+gsap.to('.box' , {
+  x: 200,
+  y: 100,
+  rotation : 180,
+  backgroundColor : 'red'
+
+
+})
+
+
+
