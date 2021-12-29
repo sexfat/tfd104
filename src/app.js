4eee86abaeba9b3a0fa716aa0d8f488fb47e1b10
sexfat <bryant6831@gmail.com>
2021-12-29 15:39:35 +0800

    add

--- src/app.js
+++ src/app.js
@@ -2,23 +2,23 @@ console.log('app ok');
 
 import './sass/main.scss';
 
-const x = (x , y) => x * y ;
-document.getElementById('app').innerHTML = x(10 , 30)
+// const x = (x , y) => x * y ;
+// document.getElementById('app').innerHTML = x(10 , 30)
 
 
-// jquery import
-import $ from 'jquery';
-$('body').css('background-color' , 'yellow');
+// // jquery import
+// import $ from 'jquery';
+// $('body').css('background-color' , 'yellow');
 
 
-//  gsap  import
-import {gsap} from 'gsap';
-gsap.to('.box' , {
-  x: 200,
-  y: 100,
-  rotation : 180,
-  backgroundColor : 'red'
-})
+// //  gsap  import
+// import {gsap} from 'gsap';
+// gsap.to('.box' , {
+//   x: 200,
+//   y: 100,
+//   rotation : 180,
+//   backgroundColor : 'red'
+// })
 
 
 // import Vue from 'vue';
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin')
 
 
 
@@ -8,7 +11,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
       index : './src/app.js' ,
-      app2 :  './src/app2.js'
+      about :  './src/app2.js'
     }, // 入口文件
     output: {
     path: path.resolve(__dirname, 'dist'),
@@ -32,6 +35,7 @@ module.exports = {
 
     },          // 處裡對應模組
     plugins: [
+        new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: "[name].css"
         }),
@@ -42,8 +46,23 @@ module.exports = {
             //來源
             filename : 'index.html'
             // 目的地
+        }),
+        new HtmlWebpackPlugin({
+            chunks : ['about'],  //選擇注入資源 chunk
+            inject  : 'head', //預設<body> js </body>  head or body
+            template : './src/about.html',
+            //來源
+            filename : 'about.html'
+            // 目的地
         })
     ],               // 對應的插件
-   // devServer: {},           // 服務器配置
-   mode: 'development'      // 開發模式配置
+    devServer: {
+        contentBase: './dist',
+        host: 'localhost',
+        port: 3600,
+        // 指定首頁檔案
+        index: 'index.html',
+        open: true
+    },         // 服務器配置
+   //mode: 'development'      // 開發模式配置
 }
